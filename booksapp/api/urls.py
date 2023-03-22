93554eb450dc2c85f3e57a2872ce6b7f14f5513d
lhoussaine HSSINI <bontop.2018@gmail.com>
2023-03-22 14:14:39 +0000

    end

--- booksapp/api/urls.py
+++ booksapp/api/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, include
-
+# from  .views import book_list
+from . import views
 from rest_framework import routers
+from rest_framework.routers import DefaultRouter
 
 rest_framework_router = routers.DefaultRouter()
 
 urlpatterns = [
     path('', include(rest_framework_router.urls)),
+    path('list_books/', views.book_list),
+    path('author_details/<int:id>/', views.author_details),
 ]
