0c6af70abe2243aefc74de3ea86e8d047fff128c
Abderrahman-byte <el.abderrahman00@gmail.com>
2023-03-22 11:03:59 +0000

    feat: add rest framework

---
+++ booksapp/api/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+
+from rest_framework import routers
+
+rest_framework_router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(rest_framework_router.urls)),
+]
